6646e9db07904141687a674334a61bcbb9654004
Raymond <yeruiminqq@gmail.com>
2014-12-09 09:12:55 +0800

    update

--- app/front_scripts/app.js
+++ app/front_scripts/app.js
@@ -1,17 +1,56 @@
+// Common Module
 var APP = {};
-APP.showTips = function(h) {
-  $('#tip-content').html(h);
-  $('#myModal').modal('show');
+
+APP.handRetCode = function(retCode, retMsg){
+  switch(retCode){
+    case 120002:
+      this.showLoginBox();
+    default:
+      this.showTips(retMsg);
+  }
+}
+
+APP.showLoginBox = function() {
+
+  var html = '<div id="j-login-modal" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header">登录</div><div class="modal-body"><div class="login-form"><label for="user">用户名：</label><input type="text"/><a href="/account/register">注册新用户</a></div><div class="login-form"><label for="pwd">登录密码：</label><input type="password"/><a href="#">找回密码</a></div><button class="btn btn-danger" id="user-login">立即登录</button></div></div></div></div>';
+
+  $('body').append(html)
+  $('#j-login-modal').modal('show')
+
+  this.bindLoginEvent()
 };
 
+APP.bindLoginEvent = function() {
+
+  $('#user-login').on('click', function(event) {
+    event.preventDefault();
+    debugger
+  });
+
+}
+
+APP.showTips = function(h) {
+
+  var html = '<div class="modal fade bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">温馨提示</h4></div><div class="modal-body text-center fc-84" id="tip-content"></div></div></div>';
+
+  $('body').append(html)
+  $('#tip-content').html(h)
+  $('#myModal').modal('show')
+
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////
+/**
+ * ALL Page Common Event
+ */
 $(function() {
 
-  // 选择彩种
+  // Header Nav Toggle
   $('#choseCai').hover(function() {
-    /* Stuff to do when the mouse enters the element */
     toggleMask($(this));
   }, function() {
-    /* Stuff to do when the mouse leaves the element */
     toggleMask($(this));
   });
 
@@ -20,4 +59,5 @@ $(function() {
     m.find('a').toggleClass('on');
   }
 
+  //APP.showLoginBox()
 });
